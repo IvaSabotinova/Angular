73d47f1c471a5f1525482bca75f866e90c90769d
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2024-03-10 14:39:40 +0200

    Select single recipe and display details

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test recipe01', 'Test01 recipe description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQ6Kw3O_4BSqCWge35jHy4zqmi1G_X5t33Sb6swTLTp6REsdFVKIPLK_onn54xLETGr8&usqp=CAU'),
     new Recipe('Test recipe02', 'Test02 recipe description', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
   ];
   constructor() { }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
