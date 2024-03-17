00d939cceffc79c2e1520581eaf04afdea697258
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2024-03-17 17:20:24 +0200

    Injecting services

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,28 +1,33 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
+ // @Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
 
   private loggingService?: LoggingService;
+  private accountService: AccountsService;
 
   constructor(){
     this.loggingService = inject(LoggingService);
+    this.accountService = inject(AccountsService);
   }
 
  // constructor(private loggingService: LoggingService) { }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
-    this.loggingService.logStatusChange(status);
+    this.accountService.updateStatus(this.id, status);
+    this.accountService.statusUpdated.emit(status);
+    //this.statusChanged.emit({ id: this.id, newStatus: status });
+   // this.loggingService.logStatusChange(status);
     // console.log('A server status changed, new status: ' + status);
   }
 }
