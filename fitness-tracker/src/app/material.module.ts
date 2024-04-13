7446869b0a39920edb18b11bb44598b8fbbbb40c
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2024-04-13 16:55:24 +0300

    Login and sidenavbar

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -6,7 +6,9 @@ import { MatInputModule } from '@angular/material/input'
 import { FlexLayoutModule } from "@angular/flex-layout";
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
-import {MatCheckboxModule} from '@angular/material/checkbox'
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -18,7 +20,9 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
         FlexLayoutModule,
         MatDatepickerModule,
         MatNativeDateModule, 
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule
     ],
     exports: [
         MatButtonModule,
@@ -28,7 +32,9 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
         FlexLayoutModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule
     ]
 })
 
